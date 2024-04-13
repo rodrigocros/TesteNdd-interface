f3ab8925e9b2b8f12067c0ef0293f116937681cf
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2024-04-13 16:11:26 -0300

    feat: header

--- ndd-teste-interface/src/app/app.component.ts
+++ ndd-teste-interface/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderMaterialUIComponent } from './Features/header-material-ui/header-material-ui.component';
 
+import { ToastrModule } from 'ngx-toastr';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderMaterialUIComponent, ToastrModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ndd-teste-interface';
+  title = 'TesteNdd-Web';
 }
